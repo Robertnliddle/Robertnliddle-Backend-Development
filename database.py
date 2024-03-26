d13a8b97e94f0117f266f30d1b66dd0d059169fe
Robertnliddle <143751397+Robertnliddle@users.noreply.github.com>
2024-03-26 17:09:30 +0100

    Add files via upload

---
+++ database.py
@@ -0,0 +1,29 @@
+def load_account():
+    return 1000
+
+
+def load_inventory():
+    return {
+        "Apple": 10,
+        "Banana": 20,
+        "Tomato": 5
+    }
+
+
+def load_history():
+    return [
+        "entry1",
+        "entry2"
+    ]
+
+
+def save_account():
+    pass
+
+
+def save_history():
+    pass
+
+
+def save_inventory():
+    pass
